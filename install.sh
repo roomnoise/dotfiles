f5ae612d60fc38f45304921159d740c96b74d114
Marcus Smith <marcus.smith@gmail.com>
2017-02-12 18:42:47 -0500

    added atom/vagrant/node installs

--- install.sh
+++ install.sh
@@ -1,3 +1,57 @@
 #!/usr/bin/env bash
 
-echo "WAZZUP"
+# Get current dir (so fun this script from anywhere...)
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXTRA_DIR="$HOME/.extra"
+
+# Update dotfiles first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Setup the symlinks
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore" ~
+
+
+###############################################################################
+# Homebrew                                                                    #
+###############################################################################
+
+. $HOME/dotfiles/install/brew.sh
+. $HOME/dotfiles/install/brew-cask.sh
+
+
+###############################################################################
+# Node                                                                        #
+###############################################################################
+
+. $HOME/dotfiles/install/npm.sh
+
+
+###############################################################################
+# Ansible                                                                     #
+###############################################################################
+
+sudo pip install ansible
+
+
+###############################################################################
+# Atom                                                                        #
+###############################################################################
+
+# Copy over Atom configs
+cp -r atom/config.cson $HOME/.atom
+cp -r atom/packages.list $HOME/.atom
+
+# Install community packages
+apm list --installed --bare # get a list of installed packages
+# todo: put a prompt for this install
+#apm install --packages-file $HOME/.atom/packages.list
+
+
+###############################################################################
+# Vagrant                                                                     #
+###############################################################################
+
+vagrant plugin install vagrant-bindfs
+vagrant plugin install vagrant-hostmanager
