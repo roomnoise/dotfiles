b6db121c0fe2222d399c5f858d2f110913eb6256
Marcus Smith <marcus.smith@gmail.com>
2017-02-25 07:53:45 -0500

    added hubstaff

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -2,10 +2,9 @@
 
 # Install Caskroom
 brew tap caskroom/cask
-brew install brew-cask
 brew tap caskroom/versions
 
-brew cask install onepassword
+brew cask install 1password
 brew cask install atom
 brew cask install charles
 brew cask install dropbox
@@ -15,7 +14,9 @@ brew cask install google-chrome
 brew cask install google-chrome-canary
 brew cask install google-drive
 brew cask install grammarly
+brew cask install hubstaff
 brew cask install iterm2
+brew cask install java
 brew cask install joinme
 brew cask install firefox
 brew cask install moom
@@ -24,10 +25,12 @@ brew cask install sequel-pro
 brew cask install sketch
 brew cask install skype
 brew cask install slack
-brew cask install spotify
+brew cask install spectacle
 brew cask install sublime-text
 brew cask install todoist
 brew cask install transmit
+brew cask install vagrant
+brew cask install virtualbox
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
