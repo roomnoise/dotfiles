4b574849b74e7c559f2e05adebd5144e64edd92d
Marcus Smith <marcus.smith@gmail.com>
2017-03-22 17:07:27 -0400

    added aws and postgres

--- install/python.sh
+++ install/python.sh
@@ -6,16 +6,25 @@
 # Ask for the administrator password upfront
 sudo -v
 
+pip install -U pip
+
 # Setup / Testing
-pip install distribute -U
+pip install distribute
 pip install nose
 pip install virtualenv
 
 # DB
 pip install cql
+pip install psycopg2
+pip install pymongo
 
 # Google APIs
 pip install google-api-python-client
 
+# AWS
+pip install awscli
+pip install boto3
+
 # Libs
 pip install pyjwt
+pip install pyopenssl
