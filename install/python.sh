93b676ad207e61bfe43c35b23cdb134c4d901d75
Marcus Smith <marcus.smith@gmail.com>
2017-12-25 12:11:30 -0500

    django and scripting libs

--- install/python.sh
+++ install/python.sh
@@ -13,7 +13,14 @@ pip install distribute
 pip install mock
 pip install nose
 pip install pytest
+pip install pytest-bdd
 pip install virtualenv
+
+# Deploy / Scripting
+pip install ansible -U
+
+# Linters
+pip install pycodestyle
 
 # DB
 pip install cql
@@ -23,19 +30,31 @@ pip install redis
 
 # Task Services
 pip install celery
+pip install celery-redbeat
 
 # Google APIs
 pip install google-api-python-client
 
+# Django
+pip install Django
+pip install dj-database-url
+pip install django-extensions
+pip install django-fsm
+pip install django-guardian
+pip install djangorestframework
+
 # AWS
 pip install awscli
-pip install boto3 -U
+pip install boto3
 
 # Stats/Errors
 pip install raven
 
 # Libs
+pip install conversion
+pip install guardian
 pip install jupyter
+pip install pandas
 pip install passlib
 pip install pyjwt
 pip install pyopenssl
@@ -43,6 +62,7 @@ pip install pytube
 pip install pyyaml
 pip install slackclient
 pip install statsd
+pip install ujson
 pip install xlrd
 
 # Utilities
